ed3e2ff1a8a89c8e84deb3b1d02727b7911e752a
lichengyin <welefen@gmail.com>
2016-06-01 10:29:42 +0800

    Save cache file with 2 depth

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {mkdir, isFile, promisify} from 'stc-helper';
+import {mkdir, isFile, promisify, md5} from 'stc-helper';
 import os from 'os';
 import path from 'path';
 import fs from 'fs';
@@ -6,6 +6,7 @@ import fs from 'fs';
 //home path
 const homePath = process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME'];
 const tmpPath = os.tmpdir();
+const noop = () => {};
 
 /**
  * cache
@@ -17,7 +18,8 @@ export default class Cache {
   constructor(options = {
     path: '',
     type: 'default',
-    handle: null
+    handle: null,
+    logger: null
   }){
     if(typeof options === 'string'){
       options = {type: options};
@@ -26,6 +28,7 @@ export default class Cache {
     this.path = this.getStorePath();
     this.cache = {};
     this.handle = options.handle || JSON;
+    this.logger = options.logger || noop;
   }
   /**
    * get store path
@@ -47,18 +50,28 @@ export default class Cache {
     }
   }
   /**
+   * get saved file path
+   */
+  getSavedFilePath(name){
+    if(!this.path){
+      return;
+    }
+    let key = md5(name);
+    let savePath = `${key[0]}/${key[1]}/${key}.json`;
+    let p = path.join(this.path, savePath);
+    mkdir(path.dirname(p));
+    return p;
+  }
+  /**
    * get cache
    */
   get(name, encoding = 'utf8'){
     if(this.cache[name]){
       return Promise.resolve(this.cache[name]);
     }
-    if(!this.path){
-      return Promise.resolve('');
-    }
-    let filePath = path.join(this.path, name);
-    if(!isFile(filePath)){
-      return Promise.resolve('');
+    let filePath = this.getSavedFilePath(name);
+    if(!filePath || !isFile(filePath)){
+      return Promise.resolve();
     }
     let fn = promisify(fs.readFile, fs);
     return fn(filePath, encoding).then(data => {
@@ -69,7 +82,7 @@ export default class Cache {
       this.cache[name] = data;
       return data;
     }).catch(() => {
-      console.log('get cache error: ', name);
+      this.logger(new Error(`get cache error ${name}`));
     });
   }
   /**
@@ -77,14 +90,14 @@ export default class Cache {
    */
   set(name, value){
     this.cache[name] = value;
-    if(!this.path){
-      return Promise.resolve('');
+    let filePath = this.getSavedFilePath(name);
+    if(!filePath){
+      return;
     }
-    let filePath = path.join(this.path, name);
     let fn = promisify(fs.writeFile, fs);
     value = this.handle.stringify(value);
     return fn(filePath, value).catch(() => {
-      console.log('set cache error: ', name);
+      this.logger(new Error(`set cache error ${name}`));
     });
   }
 }
